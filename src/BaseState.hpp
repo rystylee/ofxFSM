ed91d994b64bded4ae8f58de87edc495f72168ba
rystylee <rystylee8@gmail.com>
2019-02-27 02:36:52 +0900

    Initial commit

---
+++ src/BaseState.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace ofxFSM
+{
+
+    class BaseState
+    {
+    public:
+        virtual ~BaseState() {};
+        virtual void init(const int w, const int h) = 0;
+        virtual void start() = 0;
+        virtual void stop() = 0;
+        virtual void update() = 0;
+        virtual void updateContexts() = 0;
+        virtual void draw() = 0;
+        virtual void drawGui(const float x, const float y) = 0;
+        virtual void keyPressed(const int key) = 0;
+    };
+
+} // ofxFSM
